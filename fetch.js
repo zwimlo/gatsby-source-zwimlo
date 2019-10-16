e27ae4d4bafe1cc9b79d66b9d884c9ea4316f0b9
Harald T. Wirths <harald.wirths@4fb.de>
2019-10-16 09:06:56 +0200

    fix(deploy): change gitignore to add *.js

---
+++ fetch.js
@@ -0,0 +1,177 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _extends2 = require('babel-runtime/helpers/extends');
+
+var _extends3 = _interopRequireDefault(_extends2);
+
+var _typeof2 = require('babel-runtime/helpers/typeof');
+
+var _typeof3 = _interopRequireDefault(_typeof2);
+
+var _keys = require('babel-runtime/core-js/object/keys');
+
+var _keys2 = _interopRequireDefault(_keys);
+
+var _regenerator = require('babel-runtime/regenerator');
+
+var _regenerator2 = _interopRequireDefault(_regenerator);
+
+var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator');
+
+var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
+
+var _axios = require('axios');
+
+var _axios2 = _interopRequireDefault(_axios);
+
+var _chalk = require('chalk');
+
+var _chalk2 = _interopRequireDefault(_chalk);
+
+var _pluralize = require('pluralize');
+
+var _pluralize2 = _interopRequireDefault(_pluralize);
+
+var _normalize = require('./normalize');
+
+var _normalize2 = _interopRequireDefault(_normalize);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = function () {
+  var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(_ref2) {
+    var siteId = _ref2.siteId,
+        apiType = _ref2.apiType,
+        objectType = _ref2.objectType,
+        accessToken = _ref2.accessToken,
+        verbose = _ref2.verbose;
+    var apiEndpoint, documents;
+    return _regenerator2.default.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            console.time(_chalk2.default.cyan('zwimlo') + ' fetched ' + objectType + ' items in');
+
+            apiEndpoint = 'https://' + apiType + '.zwimlo.com/v1/sites/' + siteId + '/' + (0, _pluralize2.default)(objectType);
+            _context.next = 4;
+            return pagedGet(apiEndpoint, accessToken, objectType, verbose);
+
+          case 4:
+            documents = _context.sent;
+
+
+            console.timeEnd(_chalk2.default.cyan('zwimlo') + ' fetched ' + objectType + ' items in');
+
+            return _context.abrupt('return', {
+              documents: documents
+            });
+
+          case 7:
+          case 'end':
+            return _context.stop();
+        }
+      }
+    }, _callee, undefined);
+  }));
+
+  return function (_x) {
+    return _ref.apply(this, arguments);
+  };
+}();
+
+// Tx to https://github.com/steniowagner @ https://github.com/axios/axios/issues/604
+//  used for refDetails
+
+
+var parseParams = function parseParams(params) {
+  var keys = (0, _keys2.default)(params);
+  var options = '';
+
+  keys.forEach(function (key) {
+    var isParamTypeObject = (0, _typeof3.default)(params[key]) === 'object';
+    var isParamTypeArray = isParamTypeObject && params[key].length >= 0;
+
+    if (!isParamTypeObject) {
+      options += key + '=' + params[key] + '&';
+    }
+
+    if (isParamTypeObject && isParamTypeArray) {
+      params[key].forEach(function (element) {
+        options += key + '=' + element + '&';
+      });
+    }
+  });
+
+  return options ? options.slice(0, -1) : options;
+};
+
+var pagedGet = function () {
+  var _ref3 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee2(apiEndpoint, accessToken, objectType, verbose) {
+    var offset = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : 0;
+    var limit = arguments.length > 5 && arguments[5] !== undefined ? arguments[5] : 50;
+    var aggregatedResponse = arguments.length > 6 && arguments[6] !== undefined ? arguments[6] : [];
+    var response;
+    return _regenerator2.default.wrap(function _callee2$(_context2) {
+      while (1) {
+        switch (_context2.prev = _context2.next) {
+          case 0:
+            verbose && console.log(_chalk2.default.magentaBright('zwimlo') + ' current offset: ' + offset);
+
+            verbose && console.log(_chalk2.default.magentaBright('zwimlo') + ' limit: ' + limit);
+
+            verbose && _axios2.default.interceptors.request.use(function (request) {
+              console.log('Starting Request', request);
+              return request;
+            });
+            _context2.next = 5;
+            return _axios2.default.get(apiEndpoint, {
+              params: (0, _extends3.default)({
+                offset: offset,
+                limit: limit
+              }, accessToken ? { access_token: accessToken } : {}, objectType === 'content' ? { refDetails: ['ASSET', 'CONTENT'] } : {}),
+              paramsSerializer: function paramsSerializer(params) {
+                return parseParams(params);
+              }
+            });
+
+          case 5:
+            response = _context2.sent;
+
+
+            if (!aggregatedResponse) {
+              aggregatedResponse = response.data;
+            } else {
+              aggregatedResponse = aggregatedResponse.concat(response.data);
+            }
+
+            if (!(offset + limit < response.headers['x-total-count'])) {
+              _context2.next = 9;
+              break;
+            }
+
+            return _context2.abrupt('return', pagedGet(apiEndpoint, accessToken, objectType, verbose, offset += limit, limit, aggregatedResponse));
+
+          case 9:
+
+            aggregatedResponse = (0, _normalize2.default)(aggregatedResponse, verbose);
+
+            console.log(_chalk2.default.black.bgCyan(' zwimlo ') + ' fetched successfully ' + _chalk2.default.cyan(aggregatedResponse.length) + ' items');
+
+            return _context2.abrupt('return', aggregatedResponse);
+
+          case 12:
+          case 'end':
+            return _context2.stop();
+        }
+      }
+    }, _callee2, undefined);
+  }));
+
+  return function pagedGet(_x2, _x3, _x4, _x5) {
+    return _ref3.apply(this, arguments);
+  };
+}();
---
+++ gatsby-node.js
@@ -0,0 +1,223 @@
+'use strict';
+
+var _regenerator = require('babel-runtime/regenerator');
+
+var _regenerator2 = _interopRequireDefault(_regenerator);
+
+var _assign = require('babel-runtime/core-js/object/assign');
+
+var _assign2 = _interopRequireDefault(_assign);
+
+var _stringify = require('babel-runtime/core-js/json/stringify');
+
+var _stringify2 = _interopRequireDefault(_stringify);
+
+var _promise = require('babel-runtime/core-js/promise');
+
+var _promise2 = _interopRequireDefault(_promise);
+
+var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator');
+
+var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
+
+var _chalk = require('chalk');
+
+var _chalk2 = _interopRequireDefault(_chalk);
+
+var _crypto = require('crypto');
+
+var _crypto2 = _interopRequireDefault(_crypto);
+
+var _gatsbySourceFilesystem = require('gatsby-source-filesystem');
+
+var _lodash = require('lodash');
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
+var _fetch = require('./fetch');
+
+var _fetch2 = _interopRequireDefault(_fetch);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.sourceNodes = function () {
+  var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee2(_ref2, configOptions) {
+    var actions = _ref2.actions,
+        store = _ref2.store,
+        cache = _ref2.cache,
+        createNodeId = _ref2.createNodeId,
+        getNode = _ref2.getNode;
+
+    var createNode, createNodeField, apiType, optionAccessToken, accessToken, verbose, siteId, objectTypes, _loop, type;
+
+    return _regenerator2.default.wrap(function _callee2$(_context3) {
+      while (1) {
+        switch (_context3.prev = _context3.next) {
+          case 0:
+            createNode = actions.createNode, createNodeField = actions.createNodeField;
+
+            // Gatsby adds a configOption that's not needed for this plugin, delete it
+
+            delete configOptions.plugins;
+
+            // Consider options for delivery (content) or preview
+            apiType = configOptions.preview ? 'preview' : 'content';
+
+            // Get an existing token from options for delivery or preview
+
+            optionAccessToken = apiType === 'preview' ? configOptions.previewToken : configOptions.deliveryToken;
+
+            // Enhance the token to a parameter if available
+
+            accessToken = optionAccessToken ? optionAccessToken : '';
+
+            // Defines if the output is verbose or nearly silent
+
+            verbose = configOptions.verboseOutput;
+            siteId = configOptions.siteId;
+            objectTypes = ['asset', 'content'];
+            _loop = /*#__PURE__*/_regenerator2.default.mark(function _loop(type) {
+              var objectType, _ref3, documents;
+
+              return _regenerator2.default.wrap(function _loop$(_context2) {
+                while (1) {
+                  switch (_context2.prev = _context2.next) {
+                    case 0:
+                      objectType = objectTypes[type];
+
+                      verbose && console.log(_chalk2.default.magenta('zwimlo') + ' current objectType: ' + objectType);
+                      _context2.next = 4;
+                      return (0, _fetch2.default)({
+                        siteId: siteId,
+                        apiType: apiType,
+                        objectType: objectType,
+                        accessToken: accessToken,
+                        verbose: verbose
+                      });
+
+                    case 4:
+                      _ref3 = _context2.sent;
+                      documents = _ref3.documents;
+                      _context2.next = 8;
+                      return _promise2.default.all(documents.map(function () {
+                        var _ref4 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(node) {
+                          var nodeId, nodeType, nodeContent, nodeData, createdNode, fileNode;
+                          return _regenerator2.default.wrap(function _callee$(_context) {
+                            while (1) {
+                              switch (_context.prev = _context.next) {
+                                case 0:
+                                  nodeId = createNodeId('zwimlo-' + objectType + '-' + node.zwimlo_id);
+                                  nodeType = 'Zwimlo' + _lodash2.default.upperFirst(objectType);
+
+                                  verbose && console.log(_chalk2.default.magentaBright('zwimlo') + ' creating ' + objectType + ' with ID: ' + nodeId);
+
+                                  verbose && console.log(_chalk2.default.magentaBright('zwimlo') + ' and gatsby node type: ' + nodeType);
+                                  nodeContent = (0, _stringify2.default)(node);
+                                  nodeData = (0, _assign2.default)({}, node, {
+                                    id: nodeId,
+                                    parent: null,
+                                    children: [],
+                                    internal: {
+                                      type: nodeType,
+                                      mediaType: '' + (objectType === 'asset' ? node.file.contentType : 'application/json'),
+                                      contentDigest: _crypto2.default.createHash('md5').update(nodeContent).digest('hex')
+                                    }
+                                  });
+
+
+                                  createNode(nodeData);
+
+                                  verbose && console.log(_chalk2.default.magentaBright('zwimlo') + ' nodeData ID: ' + nodeData.id);
+                                  createdNode = getNode(nodeData.id);
+
+                                  if (!(nodeType === 'ZwimloAsset')) {
+                                    _context.next = 20;
+                                    break;
+                                  }
+
+                                  _context.next = 12;
+                                  return (0, _gatsbySourceFilesystem.createRemoteFileNode)({
+                                    url: createdNode.file.url,
+                                    store: store,
+                                    cache: cache,
+                                    createNode: createNode,
+                                    createNodeId: createNodeId
+                                  });
+
+                                case 12:
+                                  fileNode = _context.sent;
+
+
+                                  console.log('createdNode', createdNode.zwimlo_id);
+
+                                  verbose && console.log(_chalk2.default.magentaBright('zwimlo') + ' createdNode ID: ' + createdNode.id);
+
+                                  verbose && console.log(_chalk2.default.magentaBright('zwimlo') + ' fileNode ID: ' + fileNode.id);
+
+
+                                  createdNode.relativePath___NODE = fileNode.id;
+
+                                  _context.next = 19;
+                                  return createNodeField({
+                                    node: fileNode,
+                                    name: 'ZwimloAsset',
+                                    value: createdNode.zwimlo_id
+                                  });
+
+                                case 19:
+                                  verbose && console.log(_chalk2.default.magentaBright('zwimlo') + ' ' + objectType + ' node created');
+
+                                case 20:
+                                case 'end':
+                                  return _context.stop();
+                              }
+                            }
+                          }, _callee, undefined);
+                        }));
+
+                        return function (_x3) {
+                          return _ref4.apply(this, arguments);
+                        };
+                      }()), _fs2.default.writeFile('test.json', documents));
+
+                    case 8:
+                    case 'end':
+                      return _context2.stop();
+                  }
+                }
+              }, _loop, undefined);
+            });
+            _context3.t0 = _regenerator2.default.keys(objectTypes);
+
+          case 10:
+            if ((_context3.t1 = _context3.t0()).done) {
+              _context3.next = 15;
+              break;
+            }
+
+            type = _context3.t1.value;
+            return _context3.delegateYield(_loop(type), 't2', 13);
+
+          case 13:
+            _context3.next = 10;
+            break;
+
+          case 15:
+            return _context3.abrupt('return');
+
+          case 16:
+          case 'end':
+            return _context3.stop();
+        }
+      }
+    }, _callee2, undefined);
+  }));
+
+  return function (_x, _x2) {
+    return _ref.apply(this, arguments);
+  };
+}();
